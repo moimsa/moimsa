c609014e53a2b41a2a6d46a2f0c03e6ee49966bf
D.seok <ehskstkrjsa@naver.com>
2024-11-22 14:20:37 +0900

    front reset

--- frontend/src/page/Mypage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../css/page/Mypage.scss";
-
-const Mypage = () => {
-  return <div>Mypage</div>;
-};
-
-export default Mypage;
--- frontend/src/page/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NotFound = () => {
-  return <div>잘못된 경로입니다.</div>;
-};
-
-export default NotFound;
